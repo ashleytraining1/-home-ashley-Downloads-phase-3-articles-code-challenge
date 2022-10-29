2a7d674ab1ec0d5709e625e1cf4563c97fe58110
ashley <ashley.oketo@student.moringaschool.com>
2022-10-29 22:36:33 +0300

    Initialized magazine class

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,24 @@
+
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
+  end
+
+  def self.all
+    @@all
   end
 
 
 end
+
+vogue = Magazine.new("Vogue", "Beauty")
+puts vogue.name = "Elle"
+puts vogue.category = "Politics"
+puts Magazine.all
+
