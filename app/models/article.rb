e7c29ce8479cde235bcd812c11dbed07fe74b8fc
ashley <ashley.oketo@student.moringaschool.com>
2022-10-29 22:37:45 +0300

    initialized the article class

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,22 @@
-class Article
+class Article 
+    attr_reader :title
 
+    @@all = []
+    def initialize(author, magazine, title)
+            @author = author
+            @magazine = magazine
+            @title = title
+            @@all << self
+          
+    end
 
+    def self.all
+        @@all
+    end
 end
+
+beyond = Article.new("John Doe", "Vogue", "Beyond")
+puts beyond.author
+puts beyond.magazine
+puts beyond.title
+puts Article.all
